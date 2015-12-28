2e1a3f4f4861a16c126ac5f5beaa066896efbfb5
Joaquin Rodriguez Elhelou <j.rodriguez@globant.com>
2015-12-28 09:58:15 -0300

    Corrected scripts

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,51 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
 using System;
 
 public class MapGenerator : MonoBehaviour {
 
 	public int width;
 	public int height;
-	
+
 	public string seed;
 	public bool useRandomSeed;
-	
+
 	[Range(0,100)]
 	public int randomFillPercent;
-	
+
 	int[,] map;
-	
+
 	void Start() {
 		GenerateMap();
 	}
-	
+
 	void Update() {
 		if (Input.GetMouseButtonDown(0)) {
 			GenerateMap();
 		}
 	}
-	
+
 	void GenerateMap() {
 		map = new int[width,height];
 		RandomFillMap();
-		
+
 		for (int i = 0; i < 5; i ++) {
 			SmoothMap();
 		}
 
 		ProcessMap ();
 
-		int bordersize = 1;
-		int[,] borderedMap = new int[width + bordersize *2,height + bordersize * 2];
+		int borderSize = 1;
+		int[,] borderedMap = new int[width + borderSize * 2,height + borderSize * 2];
 
-		for (int x = 0; x < borderedMap.GetLength(0); x++) {
-			for (int y = 0; y < borderedMap.GetLength(1); y++) {
-				if (x >= bordersize && x < width + bordersize && y >= bordersize && y < bordersize + height) {
-					borderedMap [x, y] = map [x - bordersize, y - bordersize];
-				} else {
-					borderedMap [x, y] = 1;
+		for (int x = 0; x < borderedMap.GetLength(0); x ++) {
+			for (int y = 0; y < borderedMap.GetLength(1); y ++) {
+				if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize) {
+					borderedMap[x,y] = map[x-borderSize,y-borderSize];
+				}
+				else {
+					borderedMap[x,y] =1;
 				}
 			}
 		}
@@ -54,14 +54,14 @@ public class MapGenerator : MonoBehaviour {
 		meshGen.GenerateMesh(borderedMap, 1);
 	}
 
-	void ProcessMap(){
+	void ProcessMap() {
 		List<List<Coord>> wallRegions = GetRegions (1);
-
 		int wallThresholdSize = 50;
+
 		foreach (List<Coord> wallRegion in wallRegions) {
 			if (wallRegion.Count < wallThresholdSize) {
 				foreach (Coord tile in wallRegion) {
-					map [tile.tileX, tile.tileY] = 0;
+					map[tile.tileX,tile.tileY] = 0;
 				}
 			}
 		}
@@ -71,19 +71,20 @@ public class MapGenerator : MonoBehaviour {
 		List<Room> survivingRooms = new List<Room> ();
 
 		foreach (List<Coord> roomRegion in roomRegions) {
-			if (roomRegion.Count < wallThresholdSize) {
+			if (roomRegion.Count < roomThresholdSize) {
 				foreach (Coord tile in roomRegion) {
-					map [tile.tileX, tile.tileY] = 1;
+					map[tile.tileX,tile.tileY] = 1;
 				}
-			}else{
-				survivingRooms.Add(new Room(roomRegion,map));
+			}
+			else {
+				survivingRooms.Add(new Room(roomRegion, map));
 			}
 		}
 
-		ConnectClosestRoom (survivingRooms);
+		ConnectClosestRooms (survivingRooms);
 	}
 
-	void ConnectClosestRoom (List<Room> allRooms){
+	void ConnectClosestRooms(List<Room> allRooms) {
 
 		int bestDistance = 0;
 		Coord bestTileA = new Coord ();
@@ -94,21 +95,23 @@ public class MapGenerator : MonoBehaviour {
 
 		foreach (Room roomA in allRooms) {
 			possibleConnectionFound = false;
-			foreach(Room roomB in allRooms){
-				if(roomA == roomB){
+
+			foreach (Room roomB in allRooms) {
+				if (roomA == roomB) {
 					continue;
 				}
-				if(roomA.IsConnected(roomB)){
+				if (roomA.IsConnected(roomB)) {
 					possibleConnectionFound = false;
 					break;
 				}
-				for(int tileIndexA = 0; tileIndexA < roomA.edgeTiles.Count; tileIndexA ++){
-					for(int tileIndexB = 0; tileIndexB < roomB.edgeTiles.Count; tileIndexB ++){
+
+				for (int tileIndexA = 0; tileIndexA < roomA.edgeTiles.Count; tileIndexA ++) {
+					for (int tileIndexB = 0; tileIndexB < roomB.edgeTiles.Count; tileIndexB ++) {
 						Coord tileA = roomA.edgeTiles[tileIndexA];
 						Coord tileB = roomB.edgeTiles[tileIndexB];
-						int distanceBetweenRooms = (int)(Mathf.Pow(tileA.tileX-tileB.tileX,2) + Mathf.Pow(tileA.tileY-tileB.tileY,2));
-					
-						if(distanceBetweenRooms < bestDistance || possibleConnectionFound){
+						int distanceBetweenRooms = (int)(Mathf.Pow (tileA.tileX-tileB.tileX,2) + Mathf.Pow (tileA.tileY-tileB.tileY,2));
+
+						if (distanceBetweenRooms < bestDistance || !possibleConnectionFound) {
 							bestDistance = distanceBetweenRooms;
 							possibleConnectionFound = true;
 							bestTileA = tileA;
@@ -119,44 +122,45 @@ public class MapGenerator : MonoBehaviour {
 					}
 				}
 			}
-			if(possibleConnectionFound){
+
+			if (possibleConnectionFound) {
 				CreatePassage(bestRoomA, bestRoomB, bestTileA, bestTileB);
 			}
 		}
 	}
 
-	void CreatePassage(Room roomA, Room roomB, Coord tileA, Coord tileB){
-		Room.ConenctRooms (roomA, roomB);
-		Debug.DrawLine (CoordToWorldPoing (tileA), CoordToWorldPoing (tileB), Color.green, 100);
+	void CreatePassage(Room roomA, Room roomB, Coord tileA, Coord tileB) {
+		Room.ConnectRooms (roomA, roomB);
+		Debug.DrawLine (CoordToWorldPoint (tileA), CoordToWorldPoint (tileB), Color.green, 100);
 	}
 
-	Vector3 CoordToWorldPoing (Coord tile){
+	Vector3 CoordToWorldPoint(Coord tile) {
 		return new Vector3 (-width / 2 + .5f + tile.tileX, 2, -height / 2 + .5f + tile.tileY);
 	}
 
-	List<List<Coord>> GetRegions(int tileType){
+	List<List<Coord>> GetRegions(int tileType) {
 		List<List<Coord>> regions = new List<List<Coord>> ();
-		int[,] mapFlags = new int[width, height];
+		int[,] mapFlags = new int[width,height];
 
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				if (mapFlags [x, y] == 0 && map [x, y] == tileType) {
-					List<Coord> newRegion = GetRegionTiles (x, y);
-					regions.Add (newRegion);
+		for (int x = 0; x < width; x ++) {
+			for (int y = 0; y < height; y ++) {
+				if (mapFlags[x,y] == 0 && map[x,y] == tileType) {
+					List<Coord> newRegion = GetRegionTiles(x,y);
+					regions.Add(newRegion);
 
 					foreach (Coord tile in newRegion) {
-						mapFlags [tile.tileX, tile.tileY] = 1;
+						mapFlags[tile.tileX, tile.tileY] = 1;
 					}
 				}
 			}
 		}
-		return regions;
 
+		return regions;
 	}
 
-	List<Coord> GetRegionTiles(int startX, int startY){
+	List<Coord> GetRegionTiles(int startX, int startY) {
 		List<Coord> tiles = new List<Coord> ();
-		int[,] mapFlags = new int[width, height];
+		int[,] mapFlags = new int[width,height];
 		int tileType = map [startX, startY];
 
 		Queue<Coord> queue = new Queue<Coord> ();
@@ -164,34 +168,36 @@ public class MapGenerator : MonoBehaviour {
 		mapFlags [startX, startY] = 1;
 
 		while (queue.Count > 0) {
-			Coord tile = queue.Dequeue ();
-			tiles.Add (tile);
+			Coord tile = queue.Dequeue();
+			tiles.Add(tile);
 
 			for (int x = tile.tileX - 1; x <= tile.tileX + 1; x++) {
 				for (int y = tile.tileY - 1; y <= tile.tileY + 1; y++) {
-					if (IsInMapRange (x, y) && (y == tile.tileY || x == tile.tileX)) {
-						if (mapFlags [x, y] == 0 && map [x, y] == tileType) {
-							mapFlags [x, y] = 1;
-							queue.Enqueue (new Coord (x, y));
+					if (IsInMapRange(x,y) && (y == tile.tileY || x == tile.tileX)) {
+						if (mapFlags[x,y] == 0 && map[x,y] == tileType) {
+							mapFlags[x,y] = 1;
+							queue.Enqueue(new Coord(x,y));
 						}
 					}
 				}
 			}
 		}
+
 		return tiles;
 	}
 
-	bool IsInMapRange(int x, int y){
+	bool IsInMapRange(int x, int y) {
 		return x >= 0 && x < width && y >= 0 && y < height;
 	}
-	
+
+
 	void RandomFillMap() {
 		if (useRandomSeed) {
 			seed = Time.time.ToString();
 		}
-		
+
 		System.Random pseudoRandom = new System.Random(seed.GetHashCode());
-		
+
 		for (int x = 0; x < width; x ++) {
 			for (int y = 0; y < height; y ++) {
 				if (x == 0 || x == width-1 || y == 0 || y == height -1) {
@@ -203,26 +209,26 @@ public class MapGenerator : MonoBehaviour {
 			}
 		}
 	}
-	
+
 	void SmoothMap() {
 		for (int x = 0; x < width; x ++) {
 			for (int y = 0; y < height; y ++) {
 				int neighbourWallTiles = GetSurroundingWallCount(x,y);
-				
+
 				if (neighbourWallTiles > 4)
 					map[x,y] = 1;
 				else if (neighbourWallTiles < 4)
 					map[x,y] = 0;
-				
+
 			}
 		}
 	}
-	
+
 	int GetSurroundingWallCount(int gridX, int gridY) {
 		int wallCount = 0;
 		for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX ++) {
 			for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY ++) {
-				if (IsInMapRange(neighbourX, neighbourY)) {
+				if (IsInMapRange(neighbourX,neighbourY)) {
 					if (neighbourX != gridX || neighbourY != gridY) {
 						wallCount += map[neighbourX,neighbourY];
 					}
@@ -232,40 +238,41 @@ public class MapGenerator : MonoBehaviour {
 				}
 			}
 		}
-		
+
 		return wallCount;
 	}
 
-	struct Coord{
+	struct Coord {
 		public int tileX;
 		public int tileY;
 
-		public Coord(int x, int y){
+		public Coord(int x, int y) {
 			tileX = x;
 			tileY = y;
 		}
 	}
 
-	class Room{
+
+	class Room {
 		public List<Coord> tiles;
 		public List<Coord> edgeTiles;
 		public List<Room> connectedRooms;
 		public int roomSize;
 
-		public Room(){
+		public Room() {
 		}
 
-		public Room(List<Coord> roomTiles, int[,] map){
+		public Room(List<Coord> roomTiles, int[,] map) {
 			tiles = roomTiles;
 			roomSize = tiles.Count;
 			connectedRooms = new List<Room>();
 
 			edgeTiles = new List<Coord>();
-			foreach(Coord tile in tiles){
-				for(int x = tile.tileX-1; x<= tile.tileX +1 ; x++){
-					for (int y = tile.tileY-1; x<= tile.tileY +1 ; y++){
-						if(x==tile.tileX || y == tile.tileY){
-							if(map[x,y] == 1){
+			foreach (Coord tile in tiles) {
+				for (int x = tile.tileX-1; x <= tile.tileX+1; x++) {
+					for (int y = tile.tileY-1; y <= tile.tileY+1; y++) {
+						if (x == tile.tileX || y == tile.tileY) {
+							if (map[x,y] == 1) {
 								edgeTiles.Add(tile);
 							}
 						}
@@ -274,27 +281,13 @@ public class MapGenerator : MonoBehaviour {
 			}
 		}
 
-		public static void ConenctRooms(Room roomA, Room roomB){
+		public static void ConnectRooms(Room roomA, Room roomB) {
 			roomA.connectedRooms.Add (roomB);
 			roomB.connectedRooms.Add (roomA);
 		}
 
-		public bool IsConnected(Room otherRoom){
-			return connectedRooms.Contains (otherRoom);
+		public bool IsConnected(Room otherRoom) {
+			return connectedRooms.Contains(otherRoom);
 		}
-	}
-
-	void OnDrawGizmos() {
-		/*
-        if (map != null) {
-            for (int x = 0; x < width; x ++) {
-                for (int y = 0; y < height; y ++) {
-                    Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
-                    Vector3 pos = new Vector3(-width/2 + x + .5f,0, -height/2 + y+.5f);
-                    Gizmos.DrawCube(pos,Vector3.one);
-                }
-            }
-        }
-        */
 	}
 }
